9ca6e82812cd45c1fd685f4ded179522433b1a41
kumbrasas <kumbrasas@trulia.com>
2015-12-05 14:31:52 -0800

    progress on the ethernet system -- Kris

--- Part_2/simulation2.py
+++ Part_2/simulation2.py
@@ -1,40 +1,42 @@
 # Richard Wheeler and Kristijonas Umbrasas
 
-# Part 1: Finite Buffer Simulation
-# simulation of a simple queueing system with a finite buffer
-# to study the probability of packet loss as a function of
-# the buffer size B and the traffic intensity
+# Part 2: Binary Exponential Backoff Simulation
 
 import random
 import simpy
 import math
-import os
+import thread
+import time
+import itertools
 
-#RANDOM_SEED = 29
-#For true random:
-RANDOM_SEED = os.urandom(7)
-SIM_TIME = 1000000
+RANDOM_SEED = 29
+SIM_TIME = 100000
 MU = 1
+B = 10
 
-""" Queue system  """       
-class server_queue:
-        #initialize server
-    def __init__(self, env, Dropped_Packets, NonDropped_Packets, B, arrival_rate, Packet_Delay, Server_Idle_Periods):
-        self.server = simpy.Resource(env, capacity = 1)
-        self.env = env
-        self.queue_len = 0
-        self.buffer_max = B
-        self.flag_processing = 0
-        self.packet_number = 0
-        self.sum_time_length = 0
-        self.start_idle_time = 0
-        self.arrival_rate = arrival_rate
-        self.Packet_Delay = Packet_Delay
-        self.Dropped_Packets = Dropped_Packets
-        self.NonDropped_Packets = NonDropped_Packets
-        self.Server_Idle_Periods = Server_Idle_Periods
 
-        #process a packet
+""" 10-Queue system  """
+class ethernet_model:
+    def __init__(self, env, arrival_rate, Packet_Delay, Server_Idle_Periods):
+    server_queue queues = new server_queue[10]
+    currentSlot = 0
+    self.env = env
+
+    for num_queue in range(1,10):
+        for arrival_rate in [0.01, 0.02]:
+            env = simpy.Environment()
+            Packet_Delay = StatObject()
+            Server_Idle_Periods = StatObject()
+            queues[num_queue] = server_queue(env, arrival_rate, Packet_Delay, Server_Idle_Periods)
+        currentSlot = itertools.count().next()
+        thread.start_new_thread(updateSlot, ())
+
+        
+    def updateSlot():
+       while True:
+           currentSlot.next()
+           time.sleep(60)
+
     def process_packet(self, env, packet):
         with self.server.request() as req:
             start = env.now
@@ -42,11 +44,12 @@ class server_queue:
             yield env.timeout(random.expovariate(MU))
             latency = env.now - packet.arrival_time
             self.Packet_Delay.addNumber(latency)
+            #print("Packet number {0} with arrival time {1} latency {2}".format(packet.identifier, packet.arrival_time, latency))
             self.queue_len -= 1
             if self.queue_len == 0:
                 self.flag_processing = 0
                 self.start_idle_time = env.now
-
+                
     def packets_arrival(self, env):
         # packet arrivals 
         
@@ -54,68 +57,151 @@ class server_queue:
              # Infinite loop for generating packets
             yield env.timeout(random.expovariate(self.arrival_rate))
               # arrival time of one packet
-            if self.queue_len <= self.buffer_max:
-                self.NonDropped_Packets.addNumber(1)
-                self.packet_number += 1
-                  # packet id
-                arrival_time = env.now  
-                new_packet = Packet(self.packet_number,arrival_time)
-                if self.flag_processing == 0:
-                    self.flag_processing = 1
-                    idle_period = env.now - self.start_idle_time
-                    self.Server_Idle_Periods.addNumber(idle_period)
-                self.queue_len += 1
-                env.process(self.process_packet(env, new_packet))
-            else:
-                self.Dropped_Packets.addNumber(1)
 
+                        self.total_no_packets += 1
+                        if (self.queue_len < B) {
+                            self.packet_number += 1
+                              # packet id
+                            arrival_time = env.now  
+                            #print(self.num_pkt_total, "packet arrival")
+                            new_packet = Packet(self.packet_number,arrival_time)
+                            if self.flag_processing == 0:
+                self.flag_processing = 1
+                idle_period = env.now - self.start_idle_time
+                self.Server_Idle_Periods.addNumber(idle_period)
+                #print("Idle period of length {0} ended".format(idle_period))
+                            self.queue_len += 1
+                            env.process(self.process_packet(env, new_packet))
+                            }
+                        else {
+                            self.discards += 1
+                            }
+    
+
+
+
+class server_queue:
+    def __init__(self, env, arrival_rate, Packet_Delay, Server_Idle_Periods):
+        L = 0 #The number of packets in the queue.
+        N = 0 #The number of times the packet at the head of the queue has been retransmitted. When a new packet comes to
+            #the head of the queue n is reset to 0.
+        S = 0 #The slot number when the next transmission attempt will be made for the packet at the head of the queue.
+    
+
+        self.server = simpy.Resource(env, capacity = 1)
+        self.env = env
+        self.queue_len = 0
+        self.flag_processing = 0
+        self.packet_number = 0
+        self.sum_time_length = 0
+        self.start_idle_time = 0
+        self.arrival_rate = arrival_rate
+        self.Packet_Delay = Packet_Delay
+        self.Server_Idle_Periods = Server_Idle_Periods
+        self.total_no_packets = 0
+        self.discards = 0
+        
+    def process_packet(self, env, packet):
+        with self.server.request() as req:
+            start = env.now
+            yield req
+            yield env.timeout(random.expovariate(MU))
+            latency = env.now - packet.arrival_time
+            self.Packet_Delay.addNumber(latency)
+            #print("Packet number {0} with arrival time {1} latency {2}".format(packet.identifier, packet.arrival_time, latency))
+            self.queue_len -= 1
+            if self.queue_len == 0:
+                self.flag_processing = 0
+                self.start_idle_time = env.now
+                
+    def packets_arrival(self, env):
+        # packet arrivals 
+        
+        while True:
+             # Infinite loop for generating packets
+            yield env.timeout(random.expovariate(self.arrival_rate))
+              # arrival time of one packet
+
+                        self.total_no_packets += 1
+                        if (self.queue_len < B) {
+                            self.packet_number += 1
+                              # packet id
+                            arrival_time = env.now  
+                            #print(self.num_pkt_total, "packet arrival")
+                            new_packet = Packet(self.packet_number,arrival_time)
+                            if self.flag_processing == 0:
+                self.flag_processing = 1
+                idle_period = env.now - self.start_idle_time
+                self.Server_Idle_Periods.addNumber(idle_period)
+                #print("Idle period of length {0} ended".format(idle_period))
+                            self.queue_len += 1
+                            env.process(self.process_packet(env, new_packet))
+                            }
+                        else {
+                            self.discards += 1
+                            }
+    
 
 """ Packet class """            
 class Packet:
     def __init__(self, identifier, arrival_time):
         self.identifier = identifier
         self.arrival_time = arrival_time
+        
 
 class StatObject:
     def __init__(self):
         self.dataset =[]
+
     def addNumber(self,x):
         self.dataset.append(x)
+    def sum(self):
+        n = len(self.dataset)
+        sum = 0
+        for i in self.dataset:
+            sum = sum + i
+        return sum
+    def mean(self):
+        n = len(self.dataset)
+        sum = 0
+        for i in self.dataset:
+            sum = sum + i
+        return sum/n
+    def maximum(self):
+        return max(self.dataset)
+    def minimum(self):
+        return min(self.dataset)
     def count(self):
         return len(self.dataset)
+    def median(self):
+        self.dataset.sort()
+        n = len(self.dataset)
+        if n//2 != 0: # get the middle number
+            return self.dataset[n//2]
+        else: # find the average of the middle two numbers
+            return ((self.dataset[n//2] + self.dataset[n//2 + 1])/2)
+    def standarddeviation(self):
+        temp = self.mean()
+        sum = 0
+        for i in self.dataset:
+            sum = sum + (i - temp)**2
+        sum = sum/(len(self.dataset) - 1)
+        return math.sqrt(sum)
 
-        
+
 def main():
-    print("Finite queue system model: mu = {0}".format(MU))
+    print("Binary Exponential Backoff Model with 10 Queues: ")
     random.seed(RANDOM_SEED)
-    for B in [10, 50]:
-        print ("------ B = "+str(B)+" ------")
-        print ("{0:<15} {1:<15} {2:<15} {3:<15} {4:<15}".format(
-                "ArrivalRate", "Dropped","NonDropped","ProbabilityDropped","Theoretical Value"))
-        for arrival_rate in [0.2, 0.4, 0.6, 0.8, 0.9, 0.99]:
-            # run the sim until num.packets.in.buff == B
-            # yield to packet drop process until transmit packet,
-            # clearing buffer: |buffer| = B-1
-            env = simpy.Environment()
-            Packet_Delay = StatObject()
-            Server_Idle_Periods = StatObject()
-            Dropped_Packets = StatObject()
-            NonDropped_Packets = StatObject()
-            router = server_queue(env, Dropped_Packets, NonDropped_Packets, B, arrival_rate, Packet_Delay, Server_Idle_Periods)
-            env.process(router.packets_arrival(env))
-            env.run(until=SIM_TIME)
-            capacity = B+1
-            top = 1-(pow(arrival_rate/MU, capacity))
-            bottom = 1-(pow(arrival_rate/MU,capacity+1))
-            final = 1-(top/bottom)
-            print ("{0:<1.2f} {1:<10} {2:<15} {3:<15} {4:<1.7f} {5:<8} {6:<1.7f}".format(
-                float(arrival_rate),
-                "",
-                int(Dropped_Packets.count()),
-                int(NonDropped_Packets.count()),
-                round(Dropped_Packets.count()/float(Dropped_Packets.count()+NonDropped_Packets.count()),5),
-                "",
-                final))
-
-            
+    env = simpy.Environment()
+    env.process(router.packets_arrival(env))
+    env.run(until=SIM_TIME)
+    
 if __name__ == '__main__': main()
+
+
+
+
+
+
+
+

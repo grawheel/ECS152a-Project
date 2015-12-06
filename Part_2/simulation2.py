0030646f37e61c18470d697302d07d3209f1f7cd
kumbrasas <kumbrasas@trulia.com>
2015-12-05 21:35:49 -0800

    improper termination bug

--- Part_2/simulation2.py
+++ Part_2/simulation2.py
@@ -35,9 +35,10 @@ class server_queue:
         self.discards = 0
         self.currentPacket = 0
         
-    def process_packet(self, env, packet):
+    def process_packet(self, env):
         #print("Packet number {0} with arrival time {1} latency {2}".format(packet.identifier, packet.arrival_time, latency))
         self.queue_len -= 1
+        print("Processing for slot: " + repr(self.slotNum))
         if self.queue_len == 0:
             self.flag_processing = 0
                 
@@ -73,34 +74,35 @@ class StatObject:
 
 class ethernet_model:
     def __init__(self, env, arrival_rate, Packet_Delay, Server_Idle_Periods):
-        self.currentSlot = 0
+        self.currentSlot = 1
         self.env = env
         self.Packet_Delay = Packet_Delay
         self.Server_Idle_Periods = Server_Idle_Periods
         self.queues = [server_queue(env, arrival_rate, Packet_Delay, Server_Idle_Periods) for _ in range(10)]
-        for i in range(1,10)
-            env.process(queues[i].packets_arrival(env))
+        for i in range(0,9):
+            env.process(self.queues[i].packets_arrival(env))
+        
         
     def runModel(self,env):
-        currentSlot = 1
         while True:
             queuesThatWantToSend = []
-            # print(self.currentSlot)
+            print(self.currentSlot)
             for currentQueue in range(1,10):#check which queues want to send for current slot number
-                if (self.queues[currentQueue].queue_len) == 0
-                    self.queues[currentQueue].currentSlot = self.currentSlot #keep slot numbers current
-                if ((self.queues[currentQueue].queue_len > 0) && (self.queues[currentQueue].slotNum==currentSLot))
+                if ((self.queues[currentQueue].queue_len) == 0):
+                    self.queues[currentQueue].slotNum = self.currentSlot #keep slot numbers current
+                if ((self.queues[currentQueue].queue_len > 0) and (self.queues[currentQueue].slotNum==self.currentSlot)):
                     queuesThatWantToSend.append(currentQueue) 
 
             if(len(queuesThatWantToSend) > 1):#if collisions occur
-                self.exponentionalBackoff(self,env,queuesThatWantToSend)
-            else if(len(queuesThatWantToSend) == 1):#otherwise
+                self.exponentionalBackoff(env,queuesThatWantToSend)
+            elif(len(queuesThatWantToSend) == 1):#otherwise
                 #env.process(self.process_packet(env, new_packet))
                 self.queues[queuesThatWantToSend[0]].process_packet(env)
                 self.queues[queuesThatWantToSend[0]].N = 0
                 self.queues[queuesThatWantToSend[0]].slotNum = self.queues[queuesThatWantToSend[0]].slotNum + 1
 
-            yield env.timeout(1) # lets say that env.timeout(1) represents 1 second, which is the service time
+            yield self.env.timeout(1) # lets say that env.timeout(1) represents 1 second, which is the service time
+            self.currentSlot = self.currentSlot + 1
 
     def exponentionalBackoff(self,env,queuesThatWantToSend):#recalculate slotNum for each queueThatWantToSend
         for currentIndex in range(1,len(queuesThatWantToSend)):
